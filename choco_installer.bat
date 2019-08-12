f7ced13f408952d5dbb6e0f24798f03513338a5a
Renan <rfpe@users.noreply.github.com>
2019-08-12 13:58:52 -0300

    Update installation script
    Add and Remove applications to match my current usage

--- choco_installer.bat
+++ choco_installer.bat
@@ -8,17 +8,15 @@ choco install -y k-litecodecpackfull
 choco install -y windirstat
 
 :: Security
-choco install -y malwarebytes
+:: choco install -y malwarebytes
 choco install -y spybot
-choco install -y sandboxie
+:: choco install -y sandboxie
 
 :: Internet
 choco install -y googlechrome
-choco install -y firefox
 
 :: Backup
 choco install -y dropbox
-choco install -y icloud
 
 :: Development
 choco install -y jdk8
@@ -29,23 +27,28 @@ choco install -y git
 choco install -y emeditor
 choco install -y notepadplusplus
 choco install -y netbeans
-choco install -y putty
-choco install -y winmerge
 choco install -y virtualbox
+choco install -y vscode
+choco install -y jetbrainstoolbox
+
+## Administration
+choco install -y putty
+choco install -y winscp.install
+choco install -y winmerge
+
 
 :: Creation
 choco install -y lightworks
-choco install -y musescore
 
 :: Comm
-choco install -y skype
-choco install -y slack
+:: choco install -y skype
 
 :: Others
 choco install -y calibre
 choco install -y logitech-options
-choco install -y partitionwizard
+:: choco install -y partitionwizard
 choco install -y deluge
+choco install -y deezer
 
 :: Additional env setup
 
